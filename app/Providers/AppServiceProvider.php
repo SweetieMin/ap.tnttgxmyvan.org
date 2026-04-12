bc7f1f29e1bd85e483c1dbc94e04a070229971bb
SweetieMin <thomassmyth.isaac@gmail.com>
2026-04-12 08:11:03 +0700

    cập nhật provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +28,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        RedirectIfAuthenticated::redirectUsing(function () {
+            return route('dashboard');
+        });
+
+        Authenticate::redirectUsing(function () {
+            Session::flash('status', 'Bạn cần đăng nhập để tiếp tục');
+            return route('login');
+        });
     }
 
     /**
@@ -33,11 +46,14 @@ class AppServiceProvider extends ServiceProvider
     {
         Date::use(CarbonImmutable::class);
 
+        Schema::defaultStringLength(191);
+
         DB::prohibitDestructiveCommands(
             app()->isProduction(),
         );
 
-        Password::defaults(fn (): ?Password => app()->isProduction()
+        Password::defaults(
+            fn(): ?Password => app()->isProduction()
             ? Password::min(12)
                 ->mixedCase()
                 ->letters()
